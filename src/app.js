3196564b957bbd75d31ef7b726935b45f6450684
lnden <258231674@qq.com>
2021-04-08 15:01:04 +0800

    refactor: modify the directory, and add Router and EJS

--- src/app.js
+++ src/app.js
@@ -6,15 +6,14 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const test = require('./routes/test')
 
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -25,16 +24,15 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(test.routes(), test.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
---
+++ src/routes/test.js
@@ -0,0 +1,61 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('test', {
+    title: 'hello koa 2!',
+    msg: '你好',
+    isMe: false,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaaa'
+      },
+      {
+        id: 2,
+        title: 'bbbb'
+      },
+      {
+        id: 3,
+        title: 'cccc'
+      }
+    ]
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is loadMore API',
+    userName,
+    pageIndex
+  }
+})
+
+// router.prefix('/users') //路由前缀
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  ctx.body = {
+    userName,
+    password
+  }
+})
+
+module.exports = router
